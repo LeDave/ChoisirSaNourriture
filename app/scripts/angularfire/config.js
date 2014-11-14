2c5d5b850ac4d5ef4f50e3b997f234ce1021ba9f
LeDave <alibaba7488@outlook.com>
2014-11-14 20:35:14 +0000

    First commit

---
+++ app/scripts/angularfire/config.js
@@ -0,0 +1,2 @@
+angular.module('firebase.config', [])
+  .constant('FBURL', 'https://blistering-fire-654.firebaseio.com');
---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name workspaceApp
+ * @description
+ * # workspaceApp
+ *
+ * Main module of the application.
+ */
+angular.module('workspaceApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils'
+  ]);
